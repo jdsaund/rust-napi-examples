cd24498706a0867f034707fbe21d81d8fb53c281
Jolyon Saunders <jol.saund@gmail.com>
2020-06-21 20:05:16 +1000

    Moved get_single_param to utils

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ use nodejs_sys::{
     napi_callback_info,
     napi_create_function,
     napi_create_string_utf8,
-    napi_get_cb_info,
     napi_get_value_string_utf8,
     napi_env,
     napi_set_named_property,
@@ -11,24 +10,9 @@ use nodejs_sys::{
     napi_value,
 };
 
-// convenience wrapper to get single parameter
-fn get_single_param(env: napi_env, info: napi_callback_info) -> napi_value {
-    let mut buffer: [napi_value; 1] = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
-    let mut argc = 1 as usize;
+mod utils;
 
-    let status = unsafe { napi_get_cb_info(
-        env,
-        info,
-        &mut argc,
-        buffer.as_mut_ptr(),
-        std::ptr::null_mut(),
-        std::ptr::null_mut(),
-    ) };
-
-    assert_eq!(status, napi_status::napi_ok);
-
-    buffer[0]
-}
+use utils::get_single_param;
 
 pub extern "C" fn reverse_string(env: napi_env, info: napi_callback_info) -> napi_value {
     let napi_val: napi_value = get_single_param(env, info);
---
+++ src/utils.rs
@@ -0,0 +1,26 @@
+use nodejs_sys::{
+    napi_callback_info,
+    napi_get_cb_info,
+    napi_env,
+    napi_status,
+    napi_value,
+};
+
+// convenience wrapper to get single parameter
+pub fn get_single_param(env: napi_env, info: napi_callback_info) -> napi_value {
+    let mut buffer: [napi_value; 1] = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
+    let mut argc = 1 as usize;
+
+    let status = unsafe { napi_get_cb_info(
+        env,
+        info,
+        &mut argc,
+        buffer.as_mut_ptr(),
+        std::ptr::null_mut(),
+        std::ptr::null_mut(),
+    ) };
+
+    assert_eq!(status, napi_status::napi_ok);
+
+    buffer[0]
+}
